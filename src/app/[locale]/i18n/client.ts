dc14c59965bcc92cbbcc5c08093a12d7b7129bf4
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-13 20:32:18 +0300

    fixed  language change behavior for client components

--- src/app/[locale]/i18n/client.ts
+++ src/app/[locale]/i18n/client.ts
@@ -1,17 +1,17 @@
 "use client";
 
-import { useEffect } from "react";
-import i18next, { i18n } from "i18next";
-import {
-  initReactI18next,
-  useTranslation as useTransAlias,
-} from "react-i18next";
-import resourcesToBackend from "i18next-resources-to-backend";
+import i18next from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { type LocaleTypes, getOptions, locales } from "./settings";
+import resourcesToBackend from "i18next-resources-to-backend";
+import { useEffect } from "react";
+import { initReactI18next } from "react-i18next";
+import { LocaleTypes, getOptions, locales } from "./settings";
+import { useState } from "react";
+import { useTranslation as useTranslationOrg } from "react-i18next";
 
 const runsOnServerSide = typeof window === "undefined";
 
+//
 i18next
   .use(initReactI18next)
   .use(LanguageDetector)
@@ -23,29 +23,31 @@ i18next
   )
   .init({
     ...getOptions(),
-    lng: undefined,
+    lng: undefined, // let detect the language on client side
     detection: {
-      order: ["path"],
+      order: ["path", "htmlTag", "cookie", "navigator"],
     },
     preload: runsOnServerSide ? locales : [],
   });
 
 export function useTranslation(lng: LocaleTypes, ns: string) {
-  const translator = useTransAlias(ns);
-  const { i18n } = translator;
-
-  // if (runsOnServerSide && lng && i18n.resolvedLanguage !== lng) {
-  //   i18n.changeLanguage(lng);
-  // } else {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useCustomTranslationImplem(i18n, lng);
-  // }
-  return translator;
-}
-
-function useCustomTranslationImplem(i18n: i18n, lng: LocaleTypes) {
-  useEffect(() => {
-    if (!lng || i18n.resolvedLanguage === lng) return;
+  const ret = useTranslationOrg(ns);
+  const { i18n } = ret;
+  if (runsOnServerSide && lng && i18n.resolvedLanguage !== lng) {
     i18n.changeLanguage(lng);
-  }, [lng, i18n]);
+  } else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      if (activeLng === i18n.resolvedLanguage) return;
+      setActiveLng(i18n.resolvedLanguage);
+    }, [activeLng, i18n.resolvedLanguage]);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      if (!lng || i18n.resolvedLanguage === lng) return;
+      i18n.changeLanguage(lng);
+    }, [lng, i18n]);
+  }
+  return ret;
 }
