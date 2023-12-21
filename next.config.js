4fe3633309250f15c6030974960e5e925d49c508
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-22 00:31:58 +0300

    error fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
