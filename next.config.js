4728d0101dc906d55b253f32a6f2b2c0066495cd
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-21 23:55:13 +0300

    error fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  assetPrefix: isProd ? "https://ae-solar.vercel.app" : undefined,
+};
+
+module.exports = nextConfig;
