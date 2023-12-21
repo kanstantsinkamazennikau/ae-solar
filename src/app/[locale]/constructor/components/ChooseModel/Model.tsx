d79b5e19c9f1384f2ddc5c70fc6dc9f0b1aa1c4c
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-22 01:47:13 +0300

    error fix

--- src/app/[locale]/constructor/components/ChooseModel/Model.tsx
+++ src/app/[locale]/constructor/components/ChooseModel/Model.tsx
@@ -2,18 +2,23 @@
 
 import SectionHeader from "@/app/[locale]/constructor/components/ChooseModel/SectionHeader";
 import {
+  ConstructorContext,
+  ConstructorModel,
+} from "@/app/[locale]/context/constructorContext";
+import {
   CONSTRUCTOR_MODEL,
   CONSTRUCTOR_MODELS_SPEC,
   CONSTRUCTOR_WHICH_IS_BEST,
 } from "@/app/[locale]/utils/constants";
+import { useContext } from "react";
 
 const models = Object.keys(CONSTRUCTOR_MODELS_SPEC) as Array<
   keyof typeof CONSTRUCTOR_MODELS_SPEC
 >;
 
 export default function Model() {
-  // const { constructorModel, setConstructorModel } =
-  //   useContext(ConstructorContext);
+  const { constructorModel, setConstructorModel } =
+    useContext(ConstructorContext);
 
   return (
     <div className="flex flex-col">
@@ -25,17 +30,17 @@ export default function Model() {
         {models.map((model) => (
           <div
             key={model}
-            // className={`flex flex-col gap-2 p-4 items-start border-2 border-solid rounded-xl cursor-pointer ${
-            //   constructorModel.model === model
-            //     ? "border-base-red"
-            //     : "border-option-border "
-            // }`}
-            // onClick={() =>
-            //   setConstructorModel((prevState: ConstructorModel) => ({
-            //     ...prevState,
-            //     model,
-            //   }))
-            // }
+            className={`flex flex-col gap-2 p-4 items-start border-2 border-solid rounded-xl cursor-pointer ${
+              constructorModel.model === model
+                ? "border-base-red"
+                : "border-option-border "
+            }`}
+            onClick={() =>
+              setConstructorModel((prevState: ConstructorModel) => ({
+                ...prevState,
+                model,
+              }))
+            }
           >
             <p className="text-base font-semibold">{model}</p>
             <p className="font-walsheim text-sm leading-[1.2] font-normal">
