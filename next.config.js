abb2c283afba807d654cb3cb29fa83967ccf08ed
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-21 23:57:42 +0300

    error fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
-const nextConfig = {
-  assetPrefix: isProd ? "https://ae-solar.vercel.app" : undefined,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/app/[locale]/constructor/page.tsx
+++ src/app/[locale]/constructor/page.tsx
@@ -1,9 +1,5 @@
 import Option from "@/app/[locale]/constructor/components/Option";
 
 export default function Calculate() {
-  return (
-    <>
-      <Option />
-    </>
-  );
+  return <>{/* <Option /> */}</>;
 }
