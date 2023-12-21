e1ea77e44511560f023e3d1ca78f3d2f03c75c5f
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-22 01:21:26 +0300

    error fix

--- src/app/[locale]/constructor/components/ChooseModel/Model.tsx
+++ src/app/[locale]/constructor/components/ChooseModel/Model.tsx
@@ -1,16 +1,4 @@
-"use client";
-
-import SectionHeader from "@/app/[locale]/constructor/components/ChooseModel/SectionHeader";
-import {
-  ConstructorContext,
-  ConstructorModel,
-} from "@/app/[locale]/context/constructorContext";
-import {
-  CONSTRUCTOR_MODEL,
-  CONSTRUCTOR_MODELS_SPEC,
-  CONSTRUCTOR_WHICH_IS_BEST,
-} from "@/app/[locale]/utils/constants";
-import { useContext } from "react";
+// "use client";
 
 // const models = Object.keys(CONSTRUCTOR_MODELS_SPEC) as Array<
 //   keyof typeof CONSTRUCTOR_MODELS_SPEC
