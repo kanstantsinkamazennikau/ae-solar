56cd672230122b2838cf32819c5a469b880b45d1
kanstantsinkamazennikau <dab.sky1997@gmail.com>
2023-12-22 01:25:08 +0300

    error fix

--- src/app/[locale]/constructor/components/ChooseModel/Model.tsx
+++ src/app/[locale]/constructor/components/ChooseModel/Model.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-import { ConstructorContext } from "@/app/[locale]/context/constructorContext";
+import SectionHeader from "@/app/[locale]/constructor/components/ChooseModel/SectionHeader";
+import {
+  ConstructorContext,
+  ConstructorModel,
+} from "@/app/[locale]/context/constructorContext";
+import {
+  CONSTRUCTOR_MODEL,
+  CONSTRUCTOR_MODELS_SPEC,
+  CONSTRUCTOR_WHICH_IS_BEST,
+} from "@/app/[locale]/utils/constants";
 import { useContext } from "react";
 
 // const models = Object.keys(CONSTRUCTOR_MODELS_SPEC) as Array<
@@ -13,11 +22,11 @@ export default function Model() {
 
   return (
     <div className="flex flex-col">
-      {/* <SectionHeader
+      <SectionHeader
         title={CONSTRUCTOR_MODEL}
         clarification={CONSTRUCTOR_WHICH_IS_BEST}
       />
-      <div className="flex gap-1 flex-col">
+      {/* <div className="flex gap-1 flex-col">
         {models.map((model) => (
           <div
             key={model}
@@ -40,7 +49,6 @@ export default function Model() {
           </div>
         ))}
       </div> */}
-      re
     </div>
   );
 }
