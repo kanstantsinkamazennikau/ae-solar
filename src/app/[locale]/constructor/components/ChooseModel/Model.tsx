4d72d91698306bf7e3ab40802433a91b227fa130
kanstantsinkamazennikau <k.kamazennikau@ventionteams.com>
2023-12-22 15:11:22 +0300

    fix error

--- src/app/[locale]/constructor/components/ChooseModel/Model.tsx
+++ src/app/[locale]/constructor/components/ChooseModel/Model.tsx
@@ -1,8 +1,19 @@
-const models = ["dasd", "dadad"];
+"use client";
+
+import {
+  ConstructorContext,
+  ConstructorModel,
+} from "@/app/[locale]/context/constructorContext";
+import { CONSTRUCTOR_MODELS_SPEC } from "@/app/[locale]/utils/constants";
+import { useContext } from "react";
+
+const models = Object.keys(CONSTRUCTOR_MODELS_SPEC) as Array<
+  keyof typeof CONSTRUCTOR_MODELS_SPEC
+>;
 
 export default function Model() {
-  // const { constructorModel, setConstructorModel } =
-  //   useContext(ConstructorContext);
+  const { constructorModel, setConstructorModel } =
+    useContext(ConstructorContext);
 
   return (
     <div className="flex flex-col">
@@ -14,22 +25,22 @@ export default function Model() {
         {models.map((model) => (
           <div
             key={model}
-            // className={`flex flex-col gap-2 p-4 items-start border-2 border-solid rounded-xl cursor-pointer ${
-            //   constructorModel.model === model
-            //     ? "border-base-red"
-            //     : "border-option-border "
-            // }`}
-            // onClick={() =>
-            //   setConstructorModel((prevState: ConstructorModel) => ({
-            //     ...prevState,
-            //     model,
-            //   }))
-            // }
+            className={`flex flex-col gap-2 p-4 items-start border-2 border-solid rounded-xl cursor-pointer ${
+              constructorModel.model === model
+                ? "border-base-red"
+                : "border-option-border "
+            }`}
+            onClick={() =>
+              setConstructorModel((prevState: ConstructorModel) => ({
+                ...prevState,
+                model,
+              }))
+            }
           >
             <p className="text-base font-semibold">{model}</p>
-            {/* <p className="font-walsheim text-sm leading-[1.2] font-normal">
+            <p className="font-walsheim text-sm leading-[1.2] font-normal">
               {CONSTRUCTOR_MODELS_SPEC[model].panelTechnology}
-            </p> */}
+            </p>
           </div>
         ))}
       </div>
