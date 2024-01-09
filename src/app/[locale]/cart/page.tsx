065edda99347c7f5cf6e7a1ca11abaca78eef5cd
kanstantsinkamazennikau <k.kamazennikau@ventionteams.com>
2024-01-09 11:51:51 +0300

    started mobile version for main page

--- src/app/[locale]/cart/page.tsx
+++ src/app/[locale]/cart/page.tsx
@@ -7,10 +7,14 @@ import ModelsInCart from "@/app/[locale]/cart/component/ModelsInCart";
 import BasicWidthContainer from "@/app/[locale]/components/common/BasicWidthContainer";
 import Loader from "@/app/[locale]/components/common/Loader";
 import { ConstructorContext } from "@/app/[locale]/context/constructorContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 export default function Cart() {
-  const { isBagLoading, isShowCheckoutForm } = useContext(ConstructorContext);
+  const { isBagLoading, isShowCheckoutForm, setIsShowCheckoutForm } =
+    useContext(ConstructorContext);
+  useEffect(() => {
+    return () => setIsShowCheckoutForm(false);
+  }, [setIsShowCheckoutForm]);
 
   if (isBagLoading) return <Loader />;
 
