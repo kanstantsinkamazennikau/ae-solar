53c9d8ac44474b74b1206455c16c8e5e6ebbf960
kanstantsinkamazennikau <k.kamazennikau@ventionteams.com>
2024-01-18 18:10:26 +0300

    renamed component

--- src/app/[locale]/documents/faq/page.tsx
+++ src/app/[locale]/documents/faq/page.tsx
@@ -1,10 +1,10 @@
-import AccrodionWithIntersection from "@/app/[locale]/documents/components/AccordionWithIntersection";
+import FAQAccrodionWithIntersection from "@/app/[locale]/documents/components/FAQAccordionWithIntersection";
 import { DOCUMENTS_FAQ_FILES } from "@/app/[locale]/utils/constants";
 
 export default function FAQ() {
   return DOCUMENTS_FAQ_FILES.map(({ category, data }, index) => {
     return (
-      <AccrodionWithIntersection
+      <FAQAccrodionWithIntersection
         category={category}
         data={data}
         key={category}
