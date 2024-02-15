4bfb397d27d476c9786203185804ad729be7fe76
kanstantsinkamazennikau <k.kamazennikau@itechart-group.com>
2024-02-15 23:17:05 +0300

    fixed loader position

--- src/app/[locale]/documents/components/DocumentsLoader/index.tsx
+++ src/app/[locale]/documents/components/DocumentsLoader/index.tsx
@@ -1,6 +1,6 @@
 export default function DocumentsLoader() {
   return (
-    <div className="h-[200px] flex items-center justify-center absolute left-1/2 -translate-x-1/2">
+    <div className="h-[200px] flex items-center justify-center absolute left-1/2 -translate-x-1/2 lg:-mt-[30px] -mt-[45px]">
       <div className="flex w-8 h-8 border-solid border-base-red border-4 border-r-transparent rounded-full animate-spin"></div>
     </div>
   );
