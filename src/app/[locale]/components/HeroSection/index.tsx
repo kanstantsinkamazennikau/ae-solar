8186784b1e87a0e2ec72b03b3f7810e0cf051ef0
kanstantsinkamazennikau <k.kamazennikau@itechart-group.com>
2024-03-25 19:15:03 +0300

    hero section text position update

--- src/app/[locale]/components/HeroSection/index.tsx
+++ src/app/[locale]/components/HeroSection/index.tsx
@@ -43,7 +43,7 @@ export default function HeroSection() {
           ${
             !isStartAnimation
               ? "opacity-0 top-[50%]"
-              : "opacity-100 md:top-1/4 top-[40%]"
+              : "opacity-100 md:top-[36%] top-[40%]"
           }
         `}
       >
