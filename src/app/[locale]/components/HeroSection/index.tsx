1aa9870d312f30a6bc31570bdddcad8a540f8828
kanstantsinkamazennikau <k.kamazennikau@itechart-group.com>
2024-04-26 14:17:00 +0300

    performance test

--- src/app/[locale]/components/HeroSection/index.tsx
+++ src/app/[locale]/components/HeroSection/index.tsx
@@ -93,7 +93,7 @@ export default function HeroSection() {
             priority
             width={1920}
             height={1080}
-            className={`w-screen object-cover absolute -translate-y-1/2 top-1/2 -translate-x-1/2 left-1/2 h-screen`}
+            className={`w-full object-cover absolute -translate-y-1/2 top-1/2 -translate-x-1/2 left-1/2 h-full`}
           />
         )}
         {!isLongVideoLoadingTime && <HeroSectionVideo />}
