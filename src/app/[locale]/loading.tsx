4b99a2b156cb08340a58acec5b728f6cead8202d
kanstantsinkamazennikau <k.kamazennikau@itechart-group.com>
2024-04-26 12:49:54 +0300

    seo update

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -2,6 +2,6 @@ import Loader from "@/app/[locale]/components/common/Loader";
 
 export default function Loading() {
   return (
-    <Loader externalStyle="md:!h-[calc(100vh-130px)] h-[calc(100vh-145px)]" />
+    <Loader externalStyle="md:h-[calc(100vh-130px)] h-[calc(100vh-145px)]" />
   );
 }
